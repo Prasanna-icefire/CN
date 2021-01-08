419d28f8ba6a3fbd242ff9ab81d92f28ffd432b1
Prasanna-icefire <prasannamg3@gmail.com>
2021-01-08 21:46:10 +0530

    Completed 4th program

--- ex3/ex3.tcl
+++ ex3/ex3.tcl
@@ -4,35 +4,41 @@ $ns trace-all $tf
 set nf [open ex3.nam w]
 $ns namtrace-all $nf
 set cwind [open win3.tr w]
+
+set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
-set n6 [$ns node]
-set n7 [$ns node]
-set n8 [$ns node]
-set n9 [$ns node]
-set n10 [$ns node]
-set lan [$ns newLan "$n1 $n2 $n3 $n4 $n5 $n6 $n7 $n8 $n9 $n10 " 10Mb 2ms LL
-Queue/DropTail Channel]
-$ns duplex-link $n5 $n6 1Mb 10ms DropTail
+
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n1 $n4 1Mb 10ms DropTail
+$ns duplex-link $n4 $n5 1Mb 10ms DropTail
+$ns duplex-link $n5 $n3 1Mb 10ms DropTail
+$ns duplex-link $n3 $n2 1Mb 10ms DropTail
+$ns duplex-link $n2 $n0 1Mb 10ms DropTail
+$ns queue-limit $n1 $n4 10
+$ns queue-limit $n2 $n3 10 
+
+
 set tcp0 [new Agent/TCP]
-$ns attach-agent $n1 $tcp0
+$ns attach-agent $n0 $tcp0
 set sink0 [new Agent/TCPSink]
-$ns attach-agent $n8 $sink0
+$ns attach-agent $n4 $sink0
 $ns connect $tcp0 $sink0
 set ftp0 [new Application/FTP]
+
 $ftp0 attach-agent $tcp0
-set tcp1 [new Agent/TCP]
-$ns attach-agent $n7 $tcp1
-set sink1 [new Agent/TCPSink]
-$ns attach-agent $n3 $sink1
-$ns connect $tcp1 $sink1
-set ftp1 [new Application/FTP]
-$ftp1 attach-agent $tcp1
-$ns at 1.0 "$ftp0 start"
-$ns at 2.0 "$ftp1 start"
+$tcp0 set fid_ 1
+
+
+$ns rtmodel-at 1.0 down $n1 $n4
+$ns rtmodel-at 3.0 up $n1 $n4
+
+
+$ns at 0.1 "$ftp0 start"
+
 proc plotWindow {tcpSource file} {
 global ns
 set time 0.01
@@ -40,8 +46,8 @@ set now [$ns now]
 set cwnd [$tcpSource set cwnd_]
 puts $file "$now $cwnd"
 $ns at [expr $now+$time] "plotWindow $tcpSource $file" }
-$ns at 2.0 "plotWindow $tcp0 $cwind"
-$ns at 5.5 "plotWindow $tcp1 $cwind"
+$ns at 1.0 "plotWindow $tcp0 $cwind"
+
 proc finish {} {
 global ns tf nf cwind
 $ns flush-trace
@@ -52,5 +58,5 @@ exec xgraph win3.tr &
 exec nam ex3.nam &
 exit 0
 }
-$ns at 20.0000 "finish"
+$ns at 12.0000 "finish"
 $ns run
--- ex4/ex4.tcl
+++ ex4/ex4.tcl
@@ -5,12 +5,8 @@ $ns trace-all $tf
 
 set nf [open ex4.nam w]
 $ns namtrace-all $nf
-set cwind [open win4.tr w]
 
 $ns color 1 Blue 
-$ns color 2 Red
-
-$ns rtproto DV
 
 set n0 [$ns node]
 set n1 [$ns node]
@@ -19,6 +15,9 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 
+$n0 label "Server"
+$n5 label "Client"
+
 $ns duplex-link $n0 $n1 0.3Mb 10ms DropTail
 $ns duplex-link $n1 $n2 0.3Mb 10ms DropTail
 $ns duplex-link $n2 $n3 0.3Mb 10ms DropTail
@@ -26,39 +25,19 @@ $ns duplex-link $n1 $n4 0.3Mb 10ms DropTail
 $ns duplex-link $n3 $n5 0.5Mb 10ms DropTail
 $ns duplex-link $n4 $n5 0.5Mb 10ms DropTail
 
-$ns duplex-link-op $n0 $n1 orient right
-$ns duplex-link-op $n1 $n2 orient right
-$ns duplex-link-op $n2 $n3 orient up
-$ns duplex-link-op $n1 $n4 orient up-left
-$ns duplex-link-op $n3 $n5 orient up-left
-$ns duplex-link-op $n4 $n5 orient right-up
-
 
 set tcp [new Agent/TCP]
 $ns attach-agent $n0 $tcp
+$tcp set packetSize_ 1500
 set sink [new Agent/TCPSink]
 $ns attach-agent $n5 $sink
 $ns connect $tcp $sink
-$tcp set fid_ 1
 
 set ftp [new Application/FTP]
 $ftp attach-agent $tcp
 
-$ns rtmodel-at 1.0 down $n1 $n4
-$ns rtmodel-at 3.0 up $n1 $n4
-
 $ns at 0.1 "$ftp start"
-
 $ns at 12.0 "finish"
-
-proc plotWindow {tcpSource file} {
-global ns
-set time 0.01
-set now [$ns now]
-set cwnd [$tcpSource set cwnd_]
-puts $file "$now $cwnd"
-$ns at [expr $now+$time] "plotWindow $tcpSource $file" }
-$ns at 1.0 "plotWindow $tcp $cwind"
 
 proc finish {} {
 	global ns tf nf cwind
@@ -66,7 +45,9 @@ proc finish {} {
 	close $tf
 	close $nf
 	exec nam ex4.nam &
-        exec xgraph win4.tr & 
+	exec awk -f ex4transfer.awk ex4.tr &
+	exec awk -f ex4convert.awk ex4.tr > convert.tr &
+	exec xgraph convert.tr -geometry 800*400 -t "bytes_client" -x "time" -y "bytes_in_bps" &
 	exit 0
 }
 
